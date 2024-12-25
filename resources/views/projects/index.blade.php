722226466c9f06bd7ad21ded6e43c57c1e5c320e
zeynepoykuerdem <zeynepoykuerdem@gmail.com>
2024-12-25 19:03:27 +0100

    Modify Projects

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,9 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
+    <!-- ======= Breadcrumbs ======= -->
+    <div class="breadcrumbs d-flex align-items-center" style="background-image: {{url("public/img/breadcrumbs-bg.jpg")}};">
+        <div class="container position-relative d-flex flex-column align-items-center" data-aos="fade">
+
+            <h2>Projelerimiz</h2>
+            <ol>
+                <li><a href="{{url("/")}}">AnaSayfa</a></li>
+                <li>Projelerimiz</li>
+            </ol>
+
+        </div>
+    </div><!-- End Breadcrumbs -->
     <div class="projects-gallery py-5">
         <div class="container">
-            <h2 class="text-center mb-5">Projelerimiz</h2>
             <div class="row">
                 @foreach($projects as $project)
                     <div class="col-lg-4 col-md-6 mb-4">
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,12 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
+    <!-- ======= Breadcrumbs ======= -->
+    <div class="breadcrumbs d-flex align-items-center" style="background-image: {{url("public/img/breadcrumbs-bg.jpg")}};">
+        <div class="container position-relative d-flex flex-column align-items-center" data-aos="fade">
+
+            <h2>Projelerimiz</h2>
+
+        </div>
+    </div><!-- End Breadcrumbs -->
     <div class="project-detail py-5">
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 offset-lg-2">
                     <div class="project-item">
-                        <img src="{{ asset( $project->image_path) }}" alt="{{ $project->title }}" class="img-fluid rounded">
+                        <img src="{{ asset( $project->image_path) }}" alt="{{ $project->title }}" class="img-fluid rounded" style="width: 100%; height: auto;">
                         <div class="project-info p-3">
                             <h3 class="project-title">{{ $project->title }}</h3>
                             <p class="project-description">{{ $project->description }}</p>
